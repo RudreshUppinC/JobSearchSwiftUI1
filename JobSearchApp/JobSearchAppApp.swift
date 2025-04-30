9a20edda82f28712bd517e983001e196717deda8
Rudresh@8088 <rudreshuppinc40@gmail.com>
2025-04-30 23:03:04 +0530

    changed route to splashscreen

--- JobSearchApp/JobSearchAppApp.swift
+++ JobSearchApp/JobSearchAppApp.swift
@@ -13,8 +13,7 @@ struct JobSearchAppApp: App {
 
     var body: some Scene {
         WindowGroup {
-            MainScreenView()
-           // SplashView()
+            SplashView()
         }
 
     }
